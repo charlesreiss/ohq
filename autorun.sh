2214e76824b25c2d1444be38ba0fe03eb415a2ac
Charles Reiss <creiss@virginia.edu>
2023-08-08 11:39:44 -0400

    change autorun.sh to be directory-agnostic

--- autorun.sh
+++ autorun.sh
@@ -3,7 +3,6 @@
 # newgrp instructors # execs so lose context
 ps -A | grep ' 'cr4bd-ohq -q && exit 0
 
-cd ~/cr4bd-ohq
 if [ "$(ls -t source | head -1)" -nt ohq ]; then dub build -b release >>buildlog 2>>buildlog; fi
 date >> logs/runlog
 nohup bash restart_on_segfault.sh ./cr4bd-ohq >>logs/runlog 2>>logs/runlog </dev/null &
