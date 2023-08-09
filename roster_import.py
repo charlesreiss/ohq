82f1fca761f8a2b9327e6ef9624cc4b3d0458be3
Charles Reiss <creiss@virginia.edu>
2023-08-08 23:15:24 -0400

    also support archimedes import

--- roster_import.py
+++ roster_import.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 from pathlib import Path
 import pandas
 
@@ -53,13 +54,43 @@ def upload_csv(directory, host, course, csv):
     )
     print(r.text)
 
+def archimedes_import(input_file, directory):
+    found = set()
+    def _add(name, id, role, sections, email):
+        nonlocal found
+        found.add(id)
+        with open(directory / 'users' / (id + '.json'), 'w') as fh:
+            json.dump({
+                'name': name,
+                'id': id,
+                'role': role,
+                'sections': sections,
+                'email': email,
+            }, fp=fh)
+    read_with_callback(input_file, _add)
+    has_json = set()
+    for item in (directory / 'users').iterdir():
+        if item.suffix == 'json':
+            has_json.add(item)
+
+    for name in has_json - found:
+        print("extra user", name)
+
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--ohq-dir', default='/opt/ohq-cr4bd/logs')
     parser.add_argument('--ohq-host', default='https://kytos02.cs.virginia.edu:1112')
     parser.add_argument('--course', default='cs3130')
+    parser.add_argument('--archimedes-dir')
+    parser.add_argument('--mode', default='upload')
     parser.add_argument('input_file')
     args = parser.parse_args()
-    csv = csv_for_ohq(args.input_file)
-    upload_csv(Path(args.ohq_dir), args.ohq_host, args.course, csv)
+    if args.mode == 'archimedes':
+        archimedes_import(args.input_file, Path(args.archimedes_dir))
+    elif args.mode == 'upload':
+        csv = csv_for_ohq(args.input_file)
+        upload_csv(Path(args.ohq_dir), args.ohq_host, args.course, csv)
+    else:
+        print("unknown mode")
